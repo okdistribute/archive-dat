b763d075500a2f55ec1de5c7c135804242415b0d
Karissa McKelvey <krmckelv@gmail.com>
2016-01-12 16:26:43 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var pump = require('pump')
+var through = require('through2')
+var tar = require('tar-stream')
+
+module.exports = function (dat, link, cb) {
+  dat.joinTcpSwarm(link, function (err, link, port, close) {
+    if (err) throw err
+    var feed = dat.drive.get(link) // the link identifies/verifies the content
+    var feedStream = feed.createStream()
+    var pack = tar.pack()
+    var tarFiles = through.obj(function (entry, enc, next) {
+      entry.value.type = entry.type
+      entry.value.mtime = new Date(entry.value.mtime)
+      entry.value.ctime = new Date(entry.value.ctime)
+      var writeStream = pack.entry(entry.value, next)
+      if (!entry.link) return writeStream.end()
+      var content = dat.drive.get(entry)
+      pump(content.createStream(), writeStream, function (err) {
+        if (err) next(err)
+      })
+    })
+
+    pump(feedStream, tarFiles, function (err) {
+      if (err) return cb(err)
+      pack.finalize()
+      close()
+      return cb(null, pack)
+    })
+  })
+}
--- readme.md
+++ readme.md
@@ -1,2 +1,26 @@
 # archive-dat
-[![NPM](https://nodei.co/npm/archive-dat.png)](https://nodei.co/npm/archive-dat/)
+
+Turn a dat link into a tarball.
+
+[![Travis](http://img.shields.io/travis/karissa/archive-dat.svg?style=flat)](https://travis-ci.org/karissa/archive-dat)
+
+## Install
+
+```
+npm install archive-dat
+```
+
+## Usage
+
+```js
+var dat = require('dat')
+var archive = require('archive-dat')
+
+var db = dat()
+var link = 'dat://5aa78bedb1a1c3677725882123b08a15f48538635039c3e2096b64a13ab694b5'
+
+archive(db, link, function (err, tar) {
+  if (err) throw err
+  tar.pipe(process.stdout) // goes somewhere!
+})
+```
